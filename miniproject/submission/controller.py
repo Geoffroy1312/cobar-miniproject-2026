6fe5a843d0e753943a5ac1540ffb814337ef4d4b
Geoffroy1312 <geoffroy1312@gmail.com>
2026-04-21 17:58:40 +0200

    triangle detection for visualization

--- miniproject/submission/controller.py
+++ miniproject/submission/controller.py
@@ -1,5 +1,47 @@
 import numpy as np
 from miniproject.simulation import MiniprojectSimulation
+import cv2
+import matplotlib.pyplot as plt
+
+def detect_triangles(image):
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    
+    # 2. Extraire uniquement le canal V (Index 2)
+    # Cela donne une image en niveaux de gris basée uniquement sur la luminosité
+    v_channel = hsv_image[:, :, 2].copy() 
+    
+    # 3. Identifier les pixels verts
+    # On définit la plage du vert. On garde une saturation basse (ex: 40) 
+    # pour s'assurer de bien attraper les verts même un peu délavés.
+    lower_green = np.array([35, 40, 0])
+    upper_green = np.array([85, 255, 255])
+    
+    # green_mask vaut 255 (blanc) là où c'est vert, et 0 (noir) ailleurs
+    green_mask = cv2.inRange(hsv_image, lower_green, upper_green)
+    
+    # 4. Soustraire le vert du canal V
+    # Partout où le masque vert est activé (supérieur à 0), on force le pixel du canal V à être noir (0)
+    v_channel[green_mask <= 0] = 0
+    #augmenter la luminosité du canal V pour mieux voir les détails
+    v_channel = cv2.equalizeHist(v_channel)
+    v_channel[v_channel <= 250] = 0
+    #remove small blobs
+    #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    #v_channel = cv2.morphologyEx(v_channel, cv2.MORPH_OPEN, kernel)
+    #v_channel = cv2.morphologyEx(v_channel, cv2.MORPH_CLOSE, kernel)
+
+    #detecte les triangles 
+    contours, _ = cv2.findContours(v_channel, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    big_contours = []
+    for contour in contours:
+        epsilon = 0.04 * cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
+        if len(approx) >= 3 and cv2.contourArea(approx) > 20:  # Si le contour a 3 sommets, c'est un triangle
+            
+            #cv2.drawContours(image, [approx], 0, (255, 0, 0), 2)  # Dessine le triangle en rouge
+            big_contours.append(approx)
+    
+    return big_contours
 
 
 def odor_intensity_to_control_signal(
@@ -58,8 +100,25 @@ class Controller:
     def step(self, sim: MiniprojectSimulation):
         # implement your control algorithm here
         olfaction = sim.get_olfaction(sim.fly.name)
-
-        # get other observations as needed
+        vision = sim.get_raw_vision(sim.fly.name)
+        vision_left = vision[0]
+        vision_right = vision[1]
+        contours_left = detect_triangles(vision_left)
+        contours_right = detect_triangles(vision_right)
+        #check if a triangle bigger than a threshold is detected in the left eye, then turn left, if in the right eye, turn right
+        if len(contours_left) > 0 and len(contours_right) == 0:
+            olfaction[:, 0] *= 1.5
+            
+        elif len(contours_right) > 0 and len(contours_left) == 0:
+            olfaction[:, 0] *= 0.5
+            
+        elif len(contours_left) > 0 and len(contours_right) > 0:
+            olfaction[:, 0] *= 1.2
+            
+                
+        else:
+            olfaction[:, 0] *= 1
+            
       
 
         
